2697510f5f04773b982cd66bff940f09062d36bd
hectorborges <hectorborges94@gmail.com>
2020-02-02 12:13:11 -0600

    added new console, added some sound

--- Assets/BreakMirror.cs
+++ Assets/BreakMirror.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class BreakMirror : MonoBehaviour
 {
     public GameObject brokenModel;
+    AudioSource source;
 
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag.Equals("Finger"))
         {
+            source.Play();
             brokenModel.SetActive(true);
             gameObject.SetActive(false);
         }

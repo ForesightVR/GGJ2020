11214193377745e5383958ad3b4dfb56662cde01
hectorborges <hectorborges94@gmail.com>
2020-02-02 11:50:34 -0600

    added broken windows

---
+++ Assets/BreakMirror.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakMirror : MonoBehaviour
+{
+    public GameObject brokenModel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Finger"))
+        {
+            brokenModel.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}

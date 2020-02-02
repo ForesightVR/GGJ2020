9ed83e9bb7eea736f4fc656d55c2a2b05be35f8f
Cole Abbeduto <cole21771@gmail.com>
2020-02-01 18:36:45 -0600

    Added stuff.

---
+++ Assets/Scripts/CrystalMaker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class CrystalMaker : MonoBehaviour
+{
+    private int NUM_SHARDS = 3;
+    private int numShardsInMaker = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("CrystalShard"))
+        {
+            return;
+        }
+
+        if (++numShardsInMaker == NUM_SHARDS)
+        {
+            var pos = transform.position;
+            
+            // Instantiate(SuperShard)
+        } 
+    }
+
+    private void Update()
+    {
+        Debug.Log(numShardsInMaker);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("CrystalShard"))
+        {
+            return;
+        }
+
+        numShardsInMaker--;
+    }
+}
